e4d41f8a803d9695154e9488f33a11e10b3db43b
Mina-Khalil <mena10kh.aziz@gmail.com>
2024-07-22 10:37:20 +0300

    First version

---
+++ First_API/Controllers/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace First_API.Controllers
+{
+    [ApiController]
+    [Route("api/ [Controller]")]
+    public class Calculator : ControllerBase
+    {
+
+        [HttpGet]
+        public String CalCulator(double Number1, char N_Operation, double Number2)
+        {
+
+            while (true)
+            {
+                char choice = N_Operation;
+                double num1 = Number1;
+                double num2 = Number2;
+                double Result = 0;
+
+                switch (choice)
+                {
+                    case '+':
+                        Result = num1 + (double)Number2;
+                        return Result.ToString();
+                    case '-':
+                        Result = num1 - (double)Number2;
+                        return Result.ToString();
+                    case '*':
+                        Result = num1 * (double)Number2;
+                        return Result.ToString();
+                    case '/':
+                        if ((double)Number2 != 0)
+                        {
+                            Result = num1 / (double)Number2;
+                            return Result.ToString();
+                        }
+                        else
+                        {
+                            return ("Error: Division by zero");
+                        }
+                    default:
+                        return "you chose a mistake operation ";
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ First_API/Controllers/Hello.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace First_API.Controllers
+{
+    [ApiController]
+    [Route("api/ [Controller]")]
+    public class Hello : ControllerBase
+    {
+        [HttpGet]
+   
+        public string GetWelcome()
+        {
+            return "Hello Eng Ramez";
+        }
+
+    }
+}
